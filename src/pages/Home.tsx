43514e6a256b69a81c93a8b9e361d0b5dee03c7f
PedroABernis <pabernis@minha.fag.edu.br>
2025-03-28 20:13:07 -0300

    Primeiro commit - adicionando arquivos iniciais

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,45 +1,51 @@
-//import { useState } from "react";
+
+import { useState } from "react";
 import Logo from "../assets/logo.svg";
 import Button from "../components/Button";
 import "../styles/header.css";
 import "../styles/utility.css";
+import Close from "../assets/icons/close.svg";
+import Menu from "../assets/icons/hamburguer.svg";
+import HeroRectangleOne from "../assets/images/rectangleOne.png";
+import HeroRectangleTwo from "../assets/images/rectangleTwo.png";
+import "../styles/hero.css";
 
 export default function Home() {
-   //const [showMobileMenu, setShowMobileMenu] = useState(false);
-    return (
-         <>
-        <header className="container py-sm">
+const [showMobileMenu, setShowMobileMenu] = useState(false);
+return (
+    <>
+    <header className="container py-sm">
         <nav className="flex items-center justify-between">
-        <img src={Logo} alt="Logo DonaFrost" width={220} height={80} />
-        <div className="desktop-only">
+            <img src={Logo} alt="Logo Galpão" width={220} height={80} />
+                <div className="desktop-only">
+                <ul className="flex gap-1">
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                           <a href="#solution">Soluções</a>
+                        </li>
+                        <li>
+                            <a href="#testimonials">Depoimentos</a>
+                        </li>
+                        <li>
+                            <a href="#pricing">Preços</a>
+                        </li>
+                        <li>
+                            <a href="#contact">Contato</a>
+                        </li>
+                    </ul>
 
-    <ul className="flex gap-1">
-        <li>
-            <a href="#">Home</a>
-        </li>
-        <li>
-            <a href="#solution">Soluções</a>
-        </li>
-        <li>
-            <a href="#testimonials">Depoimentos</a>
-        </li>
-        <li>
-            <a href="#pricing">Preços</a>
-        </li>
-        <li>
-            <a href="#contact">Contato</a>
-        </li>
-    </ul>
+    </div>
 
-</div>
     <div className="desktop-only">
     <div className="flex items-center">
-        <a className="reverse-color ml-lg" href="">Login</a>
-        <Button text="Cadastre-se" />
-    </div>
+         <a className="reverse-color ml-lg" href="">Login</a>
+            <Button text="Cadastre-se" />
+        </div>
 </div>
 
-{/* <div className="mobile-menu">
+<div className="mobile-menu">
     {showMobileMenu ?
         <div className="mobile-menu-content">
             <div className="container flex">
@@ -60,23 +66,41 @@ export default function Home() {
                         <a href="#contact">Contato</a>
                     </li>
                 </ul>
-                <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper">
-                    
+                <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper">    
                     <img src={Close} alt="ícone fechar menu" width={24} height={24} />
                 </span>
             </div>
         </div>
         :
-        <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper" >
-     //
-            <img src={Menu} alt="ícone menu" width={24} height={24} />
-        </span>
+            <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper" >
+                <img src={Menu} alt="ícone menu" width={24} height={24} /> 
+            </span>
     }
-</div> */}
+    </div> 
+</nav>
 
-                 </nav>
+            
+                        </header>
 
-            </header>
+<section id="hero">
+<span className="desktop-only">
+    <img src={HeroRectangleTwo} alt="Retangulo um tela inicial" />
+</span>
+<img src={HeroRectangleOne} alt="Retangulo dois tela inicial" />
+
+<div className="container content">
+<span></span>
+<p className="desktop-only" style={{ color: "white" }}> Olá !</p>
+    <h1>Espetinhos que ardem <br></br>e cervejas que gelam!</h1>
+    <p>O sabor da brasa e a refrescância que você merece, é só pedir e aproveitar!</p>
+    <div className="flex gap-1">
+        <span><Button text="Cadastre-se" /></span>
+        <span className="desktop-only">
+            <Button text="Veja mais" secondary />
+        </span>
+    </div>
+</div>
+</section>
          </>
     )
 }
